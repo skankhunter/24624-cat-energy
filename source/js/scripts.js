9b7aa9ffd1ef0ad72d53cb5b75122941beecd710
skankhunter <intelmax3@gmail.com>
2018-02-16 21:39:03 +0300

    Правки v4.0

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -7,42 +7,44 @@ var inputTel = document.querySelector (".input__text[name='tel']");
 var inputEmail = document.querySelector (".input__text[name='email']");
 var btn = document.querySelector(".btn--sub");
 
-btn.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  if (!inputName.value) {
-    inputName.classList.add("input__text--error");
-    console.log("Нужно заполнить все поля");
-  }
-  else { delError(inputName)}
+if (btn) {
+  btn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    if (!inputName.value) {
+      inputName.classList.add("input__text--error");
+      console.log("Нужно заполнить все поля");
+    }
+    else { delError(inputName)}
 
-  if (!inputWeight.value) {
-    inputWeight.classList.add("input__text--error");
-    console.log("Нужно заполнить все поля");
-  }
+    if (!inputWeight.value) {
+      inputWeight.classList.add("input__text--error");
+      console.log("Нужно заполнить все поля");
+    }
 
-  else { delError(inputWeight)}
+    else { delError(inputWeight)}
 
-  if (!inputAge.value) {
-    inputAge.classList.add("input__text--error");
-    console.log("Нужно заполнить все поля");
-  }
+    if (!inputAge.value) {
+      inputAge.classList.add("input__text--error");
+      console.log("Нужно заполнить все поля");
+    }
 
-  else { delError(inputAge)}
+    else { delError(inputAge)}
 
-  if (!inputTel.value) {
-    inputTel.classList.add("input__text--error");
-    console.log("Нужно заполнить все поля");
-  }
+    if (!inputTel.value) {
+      inputTel.classList.add("input__text--error");
+      console.log("Нужно заполнить все поля");
+    }
 
-  else { delError(inputTel)}
+    else { delError(inputTel)}
 
-  if (!inputEmail.value) {
-    inputEmail.classList.add("input__text--error");
-    console.log("Нужно заполнить все поля");
-  }
+    if (!inputEmail.value) {
+      inputEmail.classList.add("input__text--error");
+      console.log("Нужно заполнить все поля");
+    }
 
-  else { delError(inputEmail)}
-});
+    else { delError(inputEmail)}
+  });
+}
 
 function delError (input) {
   if ( input.classList.contains("input__text--error") )
